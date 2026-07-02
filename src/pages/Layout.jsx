6118cf515c1f6ef11566c6345e1c5b38ce91b5b7
Sakshi Maurya <msakshi16264@gmail.com>
2026-07-02 14:59:25 +0530

    Add outlet

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './pages/Navbar'
-import Footer from './pages/Footer'
+import Navbar from './Navbar'
+import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
 
 
